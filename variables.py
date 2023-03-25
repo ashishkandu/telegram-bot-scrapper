696ba0802bb40ec00096ee261b10c434cdbd657a
Ashish Kandu <ashishkandu43@gmail.com>
2023-03-25 18:55:44 +0545

    Added movie api

--- variables.py
+++ variables.py
@@ -7,7 +7,8 @@ DOWNLOAD_API = "https://apis.googolplex.live/api/drive/download"
 SEARCH_API = 'https://apis.googolplex.live/api/movies/search' # add keyword at the end e.g. search/the%20vampire
 HOME_API = "https://apis.googolplex.live/api/tvSeries/fetchSeriesDetails/home"
 
-FETCH_TV_SERIES = f"https://apis.googolplex.live/api/tvSeries/fetchSeriesDetails"
+FETCH_TV_SERIES_API = "https://apis.googolplex.live/api/tvSeries/fetchSeriesDetails"
+FETCH_MOVIE_API = "https://apis.googolplex.live/api/movies/getAll"
 
 log_file_name = "bot_logs.log"
 ENV_PATH = join(CURR_PATH, '.env')
