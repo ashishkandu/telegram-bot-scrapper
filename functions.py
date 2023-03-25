12aaba96dd2c1606e9b6e0feb8d10087256f6089
Ashish Kandu <ashishkandu43@gmail.com>
2023-03-25 18:56:21 +0545

    movie support functions added

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from variables import HOME_API, ENV_PATH, SEARCH_API, FETCH_TV_SERIES, DOWNLOAD_API
+from variables import HOME_API, ENV_PATH, SEARCH_API, FETCH_TV_SERIES_API, DOWNLOAD_API, FETCH_MOVIE_API
 from browser import Browser, Response
 from requests.utils import requote_uri
 
@@ -29,8 +29,13 @@ def search_in_plex(session: Browser, keyword: str) -> Response:
     return session.get(prepared_url)
 
 
-def fetch_series(session: Browser, tv_id: str) -> Response:
-    prepared_url: str = f'{FETCH_TV_SERIES}/{tv_id}'
+def fetch_series(session: Browser, tv_id: int) -> Response:
+    prepared_url: str = f'{FETCH_TV_SERIES_API}/{tv_id}'
+    return session.get(prepared_url)
+
+
+def fetch_movie(session: Browser, movie_id: int) -> Response:
+    prepared_url: str = f'{FETCH_MOVIE_API}/{movie_id}'
     return session.get(prepared_url)
 
 
@@ -41,3 +46,25 @@ def extract_links_from_response(response_obj: Response, quality: str) -> dict:
         episodes = seasons[season]["episodes"] #List of episodes[dict] in current season
         data[season] = [f'{DOWNLOAD_API}/{episode["name"]}?id={episode["_id"]}&token=' for episode in episodes]
     return data
+
+def get_movie(response_obj: Response) -> dict:
+    response: dict = response_obj.json()
+    file_data = []
+    for file in response['files']:
+        size = round(int(file.get('size')) / 1073741824, 2) # to convert it into GB
+        movie_url = f'{DOWNLOAD_API}/{file["name"]}?id={file["_id"]}&token=' # Add token later
+        movie_links_data = {
+            'quality': file.get('quality'),
+            'size': size,
+            'language': file.get('language'),
+            'link': movie_url
+        }
+        file_data.append(movie_links_data)
+    movie_data = {
+        'poster_path': response.get('poster_path'),
+        'overview': response.get('overview'),
+        'title': response.get('title'),
+        'release_date': response.get('release_date'),
+        'link_data': file_data
+    }
+    return movie_data
